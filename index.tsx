5427137830c60f399533095d2153db2afda3feb3
Maksims Orlovs <m.orlov10@gmail.com>
2024-09-24 15:15:55 +0200

    Initial commit

---
+++ index.tsx
@@ -0,0 +1,13 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Max Orlov (cynex_)
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "autoStatus",
+    description: "Change status on different events",
+    authors: [{ name: "cynex_", id: 1234567890n }],
+});

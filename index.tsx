415244fe48eda5f61d7b361312fa3b3bbb2ffa4f
Maksims Orlovs <m.orlov10@gmail.com>
2024-09-25 13:02:33 +0200

    Add status setting

--- index.tsx
+++ index.tsx
@@ -4,7 +4,8 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import definePlugin from "@utils/types";
+import { definePluginSettings } from "@api/Settings";
+import definePlugin, { OptionType } from "@utils/types";
 import { findByCodeLazy } from "@webpack";
 import { PresenceStore, SelectedChannelStore, SelectedGuildStore, UserStore } from "@webpack/common";
 
@@ -21,6 +22,24 @@ interface VoiceState {
 const updateAsync = findByCodeLazy("updateAsync", "status"); // function that updates status
 // statuses : online, idle, dnd, invisible
 
+var settings = definePluginSettings({
+    isEnabledInCall: {
+        description: "Change status when joining a call",
+        type: OptionType.BOOLEAN,
+        default: true
+    },
+    statusInCall: {
+        description: "Status to set while in a call",
+        type: OptionType.SELECT,
+        options: [
+            { label: "Do not disturb", value: "dnd", default: true },
+            { label: "Idle", value: "idle" },
+            { label: "Invisible", value: "invisible" },
+        ],
+        disabled: () => !settings.store.isEnabledInCall
+    }
+});
+
 var prevState: VoiceState;
 var prevStatus: string;
 
@@ -28,8 +47,11 @@ export default definePlugin({
     name: "autoStatus",
     description: "Change status on different events",
     authors: [{ name: "cynex_", id: 224173920968900608n }],
+    settings,
     flux: {
         VOICE_STATE_UPDATES({ voiceStates }: { voiceStates: VoiceState[]; }) {
+            if (!settings.store.isEnabledInCall) return;
+
             const myGuildId = SelectedGuildStore.getGuildId();
             const myChanId = SelectedChannelStore.getVoiceChannelId();
             const myId = UserStore.getCurrentUser().id;
@@ -44,7 +66,7 @@ export default definePlugin({
                     const status = PresenceStore.getStatus(UserStore.getCurrentUser().id);
                     if (channelId && typeof oldChannelId === "undefined") {
                         // joining a channel
-                        updateAsync("dnd");
+                        updateAsync(settings.store.statusInCall);
                     } else if (!channelId) {
                         // leaving a channel
                         updateAsync("online");
